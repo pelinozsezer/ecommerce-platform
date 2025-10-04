09126d92bce397953c497bf387687333b02d1735
Pelin Özsezer <pelinozsezer@gmail.com>
2025-10-05 00:48:36 +0300

    chore: reset project – removed all previous files

--- ecommerce-frontend/src/components/TestComponent.jsx
+++
@@ -1,7 +0,0 @@
-export default function TestComponent() {
-  return (
-    <div className="hidden md:flex bg-red-500">
-      This should only appear on medium and larger screens.
-    </div>
-  );
-}
